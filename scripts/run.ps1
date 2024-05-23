366dec979e13bf3fb62dc2ffcfda3dc02cef576f
filippetran <filippetran@gmail.com>
2024-05-23 10:21:07 +0200

    Add mongodb compose file

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -10,15 +10,29 @@ $ProjectRoot = "${PSScriptRoot}/.."
 
 $env:FPJP_API_ENVIRONMENT="Development"
 $env:FPJP_API_PORT="8080"
+$env:FPJP_API_MONGODB_USERNAME="root"
+$env:FPJP_API_MONGODB_PASSWORD="neUhaDnes"
+
+function mongo {
+    docker compose --file ${ProjectRoot}/deployments/docker-compose/compose.yaml $args
+}
 
 switch ($command) {
-    "start" {
-        go run ${ProjectRoot}/cmd/fpjp-api-service
+  "openapi" {
+    docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
+  }
+  "start" {
+    try {
+      mongo up --detach
+      go run ${ProjectRoot}/cmd/fpjp-api-service
+    } finally {
+      mongo down
     }
-    "openapi" {
-        docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
-    }
-    default {
-        throw "Unknown command: $command"
-    }
+  }
+  "mongo" {
+    mongo up
+  }
+  default {
+    throw "Unknown command: $command"
+  }
 }
